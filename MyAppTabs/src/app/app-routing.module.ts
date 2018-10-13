3734333496ab59eadbe19d1805e20ed9987443ee
susanneffling <susan.neffling@student.laurea.fi>
2018-10-13 13:50:49 +0300

    Initial Commit

---
+++ MyAppTabs/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
